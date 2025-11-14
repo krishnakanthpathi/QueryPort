2d625aeda695122add436ce3bf6e8fa7c50dfb41
krishnakanthpathi <krishnakanthpathi@gmail.com>
2025-11-14 19:04:41 +0530

    added cors and dotenv & types

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,2 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
-console.log("Hello, TalentLayer Backend! kk");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
