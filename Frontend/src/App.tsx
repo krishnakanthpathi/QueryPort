714469229e6d2f8156aa01cc2fd78ff1c481bcdc
krishnakanthpathi <krishnakanthpathi@gmail.com>
2025-11-16 17:01:47 +0530

    added nav form react bits

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,46 @@
-import Navbar from "./components/Navbar"
-import Sidebar from "./components/Sidebar"
+import React from 'react';
+import CardNav from './components/CardNav';
+import logo from './assets/react.svg';
 
-function App() {
-  return (
-    <>
-      <Sidebar />
-      <Navbar />
-    </>
-  )
+interface NavLink {
+  label: string;
+  ariaLabel: string;
+  href: string;
 }
 
-export default App
+interface NavItem {
+  label: string;
+  bgColor: string;
+  textColor: string;
+  links: NavLink[];
+}
+
+const App: React.FC = () => {
+  const items: NavItem[] = [
+    {
+      label: "Contact",
+      bgColor: "#000", 
+      textColor: "#fff",
+      links: [
+        { label: "Email", ariaLabel: "Email us", href: "mailto:info@example.com" },
+        { label: "Twitter", ariaLabel: "Twitter", href: "https://twitter.com/YourHandle" },
+        { label: "LinkedIn", ariaLabel: "LinkedIn", href: "https://www.linkedin.com/company/your-company" }
+      ]
+    }
+  ];
+
+  return (
+    <CardNav
+      logo={logo}
+      logoAlt="Company Logo"
+      items={items}
+      baseColor="#fff"
+      menuColor="#000"
+      buttonBgColor="#111"
+      buttonTextColor="#fff"
+      ease="power3.out"
+    />
+  );
+};
+
+export default App;
--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,162 +1,17 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import LayersIcon from '@mui/icons-material/Layers';
+import React from 'react';
 
-const pages = ['Portfolio\'s', 'Api'];
-const settings = ['Profile', 'Logout'];
-
-function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+const Navbar: React.FC = () => {
   return (
-    <AppBar position="static" 
-      color="transparent"
-
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* // logo in desktop */}
-          <LayersIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontWeight: 700,
-              textDecoration: 'none',
-            }}
-          >
-            TalentLayer
-          </Typography>
-          
-          {/* // menu icon in mobile */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-          {/* // logo in mobile */}
-          <LayersIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              fontWeight: 700,
-              textDecoration: 'none',
-            }}
-          >
-            TalentLayer
-          </Typography>
-
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }  } }>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <nav className="bg-gray-800 p-4">
+      <div className="text-white text-lg font-bold">TalentLayer</div>
+      <ul className="flex space-x-4">
+        <li><a className="text-white hover:text-gray-400" href="/login">Login</a></li>
+        <li><a className="text-white hover:text-gray-400" href="/signup">Signup</a></li>
+        <li><a className="text-white hover:text-gray-400" href="/portfolio1">Portfolio 1</a></li>
+        <li><a className="text-white hover:text-gray-400" href="/portfolio2">Portfolio 2</a></li>
+      </ul>
+    </nav>
   );
-}
+};
+
 export default Navbar;
