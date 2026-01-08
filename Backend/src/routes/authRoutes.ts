f9556cc77c7fe279b901de5a1b2ff5b73c0da829
krishnakanthpathi <krishnakanthpathi@gmail.com>
2026-01-08 22:21:25 +0530

    adjust:added intro to auth route

--- Backend/src/routes/authRoutes.ts
+++ Backend/src/routes/authRoutes.ts
@@ -3,6 +3,9 @@ import { signup, login, googleAuth } from '../controllers/authController.js';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send(JSON.stringify({ message: 'Welcome to the Auth Routes' }));
+});
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/google', googleAuth);
