6d1765d8f235331b4f8c0be324c483a324150944
krishnakanthpathi <krishnakanthpathi@gmail.com>
2026-01-12 09:40:06 +0530

    feat:created routes for projects

--- Backend/src/routes/projectsRoutes.ts
+++ Backend/src/routes/projectsRoutes.ts
@@ -3,8 +3,31 @@ import express from "express";
 
 const router = express.Router();
 
+
+// create project
+router.post("/", (req, res) => {
+    res.send("Projects");
+});
+
+// get all projects
 router.get("/", (req, res) => {
     res.send("Projects");
 });
+
+// get project by id
+router.get("/:id", (req, res) => {
+    res.send("Projects");
+});
+
+// update project
+router.put("/:id", (req, res) => {
+    res.send("Projects");
+});
+
+// delete project
+router.delete("/:id", (req, res) => {
+    res.send("Projects");
+});
+
 
 export default router;
